c913e006798d1f65f7fa4d0141194b5765e93e19
Rabie TAWIL <rabietawil@gmail.com>
2019-07-19 19:06:12 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -59,3 +59,18 @@ function showRepositories() {
 //   req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
 //   req.send();
 // }
+
+function showCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits
+    .map(
+      commit =>
+        '<li><strong>' +
+        commit.author.login +
+        '</strong> - ' +
+        commit.commit.message +
+        '</li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('commits').innerHTML = commitsList;
+}
