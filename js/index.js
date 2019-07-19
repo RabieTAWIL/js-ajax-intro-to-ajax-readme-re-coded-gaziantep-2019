7194566126c279cf1159321b01e68a5579bd6223
Rabie TAWIL <rabietawil@gmail.com>
2019-07-19 18:58:56 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -17,15 +17,15 @@ function getRepositories() {
   req.send();
 }
 
-// function showRepositories() {
-//   console.log(this.responseText);
-//   let repoList = '<ul>';
-//   for (var i = 0; i < this.responseText.length; i++) {
-//     repoList += '<li>' + this.responseText[i]['name'] + '</li>';
-//   }
-//   repoList += '</ul>';
-//   document.getElementById('repositories').innerHTML = repoList;
-// }
+function showRepositories() {
+  console.log(this.responseText);
+  let repoList = '<ul>';
+  for (var i = 0; i < this.responseText.length; i++) {
+    repoList += '<li>' + this.responseText[i]['name'] + '</li>';
+  }
+  repoList += '</ul>';
+  document.getElementById('repositories').innerHTML = repoList;
+}
 
 function showRepositories() {
   var repos = JSON.parse(this.responseText);
